e6c3b2d66e6d33f3b23ca8f882adf5381a160009
josephkeelagher <KeelagherJ@gmail.com>
2024-04-25 23:59:14 +1000

    Attempted GaussianNB, not working yet

--- MLproject2.py
+++ MLproject2.py
@@ -1,7 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 def main():
-    # Saved for later
-    pass
+    df = pd.read_csv('train_dataset.csv')
+    print(df.head())
+
+    X, y = df.iloc[:, :-1], df.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    NBclassifier = GaussianNB()
+
+    NBclassifier.fit(X_train, y_train)
+
+    y_pred = NBclassifier.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    print("Accuracy: ", accuracy)
+
+
+
+if __name__ == "__main__":
+    main()
